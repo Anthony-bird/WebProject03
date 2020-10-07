d61e74c70e36c10dd7c469656f2075fb44b75dd1
Anthony-bird <61493124+Anthony-bird@users.noreply.github.com>
2020-10-08 01:04:15 +0800

    第5天1

---
+++ src/main/java/org/feng/framework/ConfigConstant.java
@@ -0,0 +1,13 @@
+package org.feng.framework;
+
+public interface ConfigConstant {
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "simple.framework.jdbc.driver";
+    String JDBC_URL = "simple.framework.jdbc.url";
+    String JDBC_USERNAME = "simple.framework.jdbc.username";
+    String JDBC_PASSWORD = "simple.framework.jdbc.password";
+    String APP_BASE_PACKAGE = "simple.framework.jdbc.base_package";
+    String APP_JSP_PATH = "simple.framework.jdbc.jsp_path";
+    String APP_ASSET_PATH = "simple.framework.jdbc.asset_path";
+}
